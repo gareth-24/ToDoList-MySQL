10063be01973315139a2989bcc61bd6cf2dc2dd7
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-15 13:48:53 -0700

    add css stylesheet and shared layout

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -17,7 +17,7 @@ namespace ToDoList
       WebApplication app = builder.Build();
 
       app.UseHttpsRedirection();
-
+      app.UseStaticFiles();
       app.UseRouting();
 
       app.MapControllerRoute(
